8ac4a889993f69473793d04a595d15e1f16f7522
John Lusky <lusky@1trickpony.com>
2016-05-17 23:46:02 -0400

    Initial Commit

---
+++ inc/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Distributors</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/styles.css">
+
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li class="logo"><a href="index.php" title="Home">MD</a></li>
+            <li><a href="#about" title="About Us">About Us</a></li>
+            <li><a href="#products" title="Products">Products</a></li>
+            <li><a href="#projects" title="Projects">Projects</a></li>
+            <li><a href="#contact" title="Contact Us">Contact Us</a></li>
+            <li class="call-us"><a href="tel:6105555555" title="Call Us Now">Call Us Today! 610-555-5555</a></li>
+        </ul>
+    </nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require'inc/header.php'; ?>
+
+    <section id="hero">
+        <div class="bubble">
+            <div class="wrap">
+                <h1>Material Distributors, Inc. <span>Quality Products on Time</span></h1>
+                <p>Since 1947, Material Distributors has been a reliable source of Division 10, 11, and 12 products to our contractor clients...supplying them with materials from over 300 manufacturers.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="content">
+        <div class="wrap">
+
+        </div>
+    </section>
+
+<?php require'inc/footer.php'; ?>
