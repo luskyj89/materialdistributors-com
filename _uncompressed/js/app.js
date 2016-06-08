d08abd0caa44900f5ed4aa1fe86e5cbbf3c3e813
John Lusky <lusky@1trickpony.com>
2016-06-07 22:53:55 -0400

    Responsive nav, cleaned files

--- _uncompressed/js/app.js
+++ _uncompressed/js/app.js
@@ -1,7 +1,19 @@
 /* ----------------------------------------
    Public Vars
 --------------------------------------- */
+var controlNav          = $("#control-icon"),
+    menuItem            = $(".menu-item");
 
+function setNav() {
+    if ( !controlNav.hasClass("active") ) {
+        controlNav.addClass("active");
+        menuItem.slideDown();
+    }
+    else {
+        controlNav.removeClass("active");
+        menuItem.slideUp();
+    }
+}
 
 /* ----------------------------------------
    Init
@@ -9,8 +21,26 @@
 
 function init() {
 
+    controlNav.click(function(e){
+        e.preventDefault();
+
+        setNav();
+    });
 
 }
+
+function resizer() {
+
+    if ( $(window).width() > 930 ) {
+        menuItem.css("display", "block");
+        controlNav.addClass("active");
+    }
+
+}
+
+$(window).resize(function(){
+    resizer();
+});
 
 $(document).ready(function(){
 
--- inc/header.php
+++ inc/header.php
@@ -21,10 +21,12 @@
     <nav>
         <ul>
             <li class="logo"><a href="index.php" title="Home">MD</a></li>
-            <li><a href="#about" title="About Us">About Us</a></li>
-            <li><a href="#products" title="Products">Products</a></li>
-            <li><a href="#projects" title="Projects">Projects</a></li>
-            <li><a href="#contact" title="Contact Us">Contact Us</a></li>
-            <li class="call-us"><a href="tel:6105555555" title="Call Us Now">Call Us Today! 610-555-5555</a></li>
+            <li class="menu-item"><a href="#about" title="About Us">About Us</a></li>
+            <li class="menu-item"><a href="#products" title="Products">Products</a></li>
+            <li class="menu-item"><a href="#projects" title="Projects">Projects</a></li>
+            <li class="menu-item"><a href="#contact" title="Contact Us">Contact Us</a></li>
+            <li class="call-us menu-item"><a href="tel:6105555555" title="Call Us Now">Call Us Today! 610-555-5555</a></li>
         </ul>
+
+        <a id="control-icon" class="icon-menu" href="#"></a>
     </nav>
